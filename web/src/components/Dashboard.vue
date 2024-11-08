065cff3321e98be9998eb0423862034dfede90b3
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-11-08 13:55:33 -0300

    ✨ Transactions table

--- web/src/components/Dashboard.vue
+++ web/src/components/Dashboard.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
-import Summary from "./Summary.vue"
+import Summary from "./Summary.vue";
+import TransactionsTable from "./TransactionsTable.vue";
 </script>
 
 <template>
   <div class="dashboard__container">
     <Summary />
+    <TransactionsTable />
   </div>
 </template>
 
 <style lang="scss" scoped>
-  .dashboard__container {
-    max-width: 1120px;
-    margin: 0 auto;
-    padding: 2.5rem 1rem;
-  }
+.dashboard__container {
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 2.5rem 1rem;
+}
 </style>
---
+++ web/src/components/TransactionsTable.vue
@@ -0,0 +1,100 @@
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TransactionsTable",
+  setup() {
+    const transactions = [
+      {
+        id: "1",
+        title: "Petshop",
+        type: "withdraw",
+        amount: 300,
+        category: "Zeus",
+        createdAt: new Date().toISOString(),
+      },
+      {
+        id: "2",
+        title: "Freela",
+        type: "deposit",
+        amount: 10000,
+        category: "Freela",
+        createdAt: new Date().toISOString(),
+      },
+    ];
+    return {
+      transactions,
+    };
+  },
+});
+</script>
+
+<template>
+  <div class="table__container">
+    <table>
+      <thead>
+        <tr>
+          <th>Título</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr :key="{ index }" v-for="(item, index) in transactions">
+          <td>{{ item.title }}</td>
+          <td>
+            {{
+              new Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              }).format(item.amount)
+            }}
+          </td>
+          <td>{{ item.category }}</td>
+          <td>{{ new Intl.DateTimeFormat("pt-BR").format(new Date(item.createdAt)) }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.table__container {
+  margin-top: 4rem;
+
+  table {
+    width: 100%;
+    border-spacing: 0 0.5rem;
+
+    th {
+      color: var(--text-body);
+      font-weight: 400;
+      padding: 1rem 2rem;
+      text-align: left;
+      line-height: 1.5rem;
+    }
+
+    td {
+      padding: 1rem 2rem;
+      border: 0;
+      background: var(--shape);
+      color: var(--text-body);
+      border-radius: 0.25rem;
+
+      &:first-child {
+        color: var(--text-title);
+      }
+
+      &.deposit {
+        color: var(--green);
+      }
+
+      &.withdraw {
+        color: var(--red);
+      }
+    }
+  }
+}
+</style>
