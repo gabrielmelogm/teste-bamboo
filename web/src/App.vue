105d51ce3cae8b0f933a511f9eaa99942cf38450
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-11-08 13:12:39 -0300

    ✨ Header component

---
+++ web/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <Header />
+</template>
+
---
+++ web/src/components/Header.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<header class="container">
+		<div class="content">
+			<h1 class="header__title">
+				<img src="../assets/logo-only.svg" alt="Logo" />
+				<span>Transações</span>
+			</h1>
+			<div>
+				<button type="button" class="header__button">Nova transação</button>
+			</div>
+		</div>
+	</header>
+</template>
+
+<style lang="scss" scoped>
+.container {
+	background: var(--blue);
+}
+
+.content {
+	max-width: 1120px;
+	margin: 0 auto;
+	padding: 2rem 1rem 12rem;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+}
+.header__title {
+	display: flex;
+	align-items: center;
+	gap: 0.5rem;
+	font-size: 1.5rem;
+}
+.header__button {
+	font-size: 1rem;
+	color: #fff;
+	background: var(--blue-light);
+	border: 0;
+	padding: 0 2rem;
+	border-radius: 0.25rem;
+	height: 3rem;
+	transition: filter 0.2s;
+
+	&:hover {
+		filter: brightness(0.9);
+		cursor: pointer;
+	}
+}
+</style>
