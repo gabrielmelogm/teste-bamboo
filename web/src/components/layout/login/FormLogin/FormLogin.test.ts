21e783dab36509a49c96b7c75cfde4a732a3471b
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-11-10 17:01:45 -0300

    ✅ Test submit form

--- web/src/components/layout/login/FormLogin/FormLogin.test.ts
+++ web/src/components/layout/login/FormLogin/FormLogin.test.ts
@@ -1,5 +1,5 @@
-import { describe, it, expect } from 'vitest'
-import { mount } from '@vue/test-utils'
+import { describe, it, expect, vi } from 'vitest'
+import { DOMWrapper, mount } from '@vue/test-utils'
 import FormLogin from './FormLogin.vue'
 
 describe('FormLogin', () => {
@@ -13,5 +13,25 @@ describe('FormLogin', () => {
 
     expect(usernameInput.exists()).toBe(true)
     expect(passwordInput.exists()).toBe(true)
+  })
+  it('deve preencher os dados corretamente e realizar submit do formulário', async () => {
+    const element = mount(FormLogin)
+
+    const usernameInput = element.find('input[name="username"]') as DOMWrapper<HTMLInputElement>
+    const passwordInput = element.find('input[name="password"]') as DOMWrapper<HTMLInputElement>
+
+    await usernameInput.setValue('admin')
+    await passwordInput.setValue('password')
+
+    expect(usernameInput.element.value).toBe('admin')
+    expect(passwordInput.element.value).toBe('password')
+
+    const button = element.find('button')
+    expect(button.text()).toContain('Login')
+
+    const submitFormSpy = vi.spyOn(element.vm, 'handleSubmit')
+    const form = element.find('form')
+    await form.trigger('submit')
+    expect(submitFormSpy).toHaveBeenCalled()
   })
 })
